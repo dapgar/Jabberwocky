6e506989bf0ee6b5c907f02e88ee22d794665d18
AJKaz <ajkazazian@gmail.com>
2024-02-21 10:46:27 -0500

    basic sword pull - no assets

--- Assets/Scripts/Minigames/SwordInStone/SIS_Character.cs
+++ Assets/Scripts/Minigames/SwordInStone/SIS_Character.cs
@@ -1,14 +1,56 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SIS_Character : MonoBehaviour {
     private float clickCooldown;
     private float cooldownTimer = 0f;
+    private bool bOnCooldown = false;
+    private int playerIndex;
+    private Key key;
 
-    public void SetupPlayer(float clickCooldown) {
+    public bool OnCooldown { get { return bOnCooldown; } }
+
+    public void SetupPlayer(float clickCooldown, int playerIndex) {
         this.clickCooldown = clickCooldown;
-    }
-    
-    public void CheckClick() {
+        this.playerIndex = playerIndex;
 
+
+        SetClickKey();
+    }
+
+    private void SetClickKey() {
+        if (playerIndex == 0) {
+            key = Key.Q;
+        }
+        else if (playerIndex == 1) {
+            key = Key.R;
+        }
+        else if (playerIndex == 2) {
+            key = Key.U;
+        }
+        else {
+            key = Key.P;
+        }
+    }
+
+    public bool CheckClick() {
+        if (bOnCooldown) {
+            cooldownTimer += Time.deltaTime;
+            if (cooldownTimer >= clickCooldown) {
+                cooldownTimer = 0f;
+                bOnCooldown = false;
+            }
+        }
+        else {
+            Keyboard keyboard = Keyboard.current;
+            if (keyboard == null) return false; // if no keyboard
+
+            if (keyboard[key].isPressed) {
+                // Click key here
+                bOnCooldown = true;
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Assets/Scripts/Minigames/SwordInStone/SIS_Manager.cs
+++ Assets/Scripts/Minigames/SwordInStone/SIS_Manager.cs
@@ -10,26 +10,33 @@ public class SIS_Manager : MonoBehaviour {
     private SIS_Character[] players;
     [SerializeField]
     private float clickCooldown = 1f;
+    [SerializeField]
+    private float maxPullLength = 0.9f;
 
     [SerializeField]
     private GameObject Sword;
 
     /* Amount that each click raises the sword */
-    private int clickPower = 1;
-
     private int totalSwordPulls;
+    private float pullAmount;
 
     private void Start() {
         totalSwordPulls = Random.Range(minSwordPulls, maxSwordPulls + 1);
+        pullAmount = maxPullLength / totalSwordPulls;
 
         for (int i = 0; i < players.Length; i++) {
-            players[i].SetupPlayer(clickCooldown);
+            players[i].SetupPlayer(clickCooldown, i);
         }
+
     }
 
     private void Update() {
         for (int i = 0; i < players.Length;i++) {
-            players[i].CheckClick();
+            if (players[i].CheckClick()) {
+                Vector3 newSwordTransform = Sword.transform.position;
+                newSwordTransform.y += pullAmount;
+                Sword.transform.position = newSwordTransform;
+            }
         }
     }
 }
