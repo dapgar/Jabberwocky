43ab03421320a741711506b702f49105c6a066a2
zjh6792 <zjh6792@g.rit.edu>
2024-02-29 14:25:27 -0500

    added player getter to the spawn initializer

--- Assets/Scripts/PlayerInput/InitializeSpawns.cs
+++ Assets/Scripts/PlayerInput/InitializeSpawns.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InitializeSpawns : MonoBehaviour
 {
@@ -8,6 +9,15 @@ public class InitializeSpawns : MonoBehaviour
     private Transform[] playerSpawns;
     [SerializeField]
     private GameObject playerPrefab;
+
+    private List<GameObject> players;
+
+    public List<GameObject> GetPlayers()
+    {
+        return players;
+    }
+
+    public UnityEvent onPlayersSpawned;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +27,12 @@ public class InitializeSpawns : MonoBehaviour
         {
             var player = Instantiate(playerPrefab, playerSpawns[i].position, playerSpawns[i].rotation, gameObject.transform);
             player.GetComponent<PlayerInputHandler>().InitializePlayer(playerConfigs[i]);
+
+            players.Add(player);
         }
+
+        // once all the players have spawned
+        onPlayersSpawned?.Invoke();
     }
 
     // Update is called once per frame
