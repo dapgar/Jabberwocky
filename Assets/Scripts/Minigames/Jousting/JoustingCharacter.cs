62068d9bf9181aebb1e19ed785392911cd6f9265
AJKaz <ajkazazian@gmail.com>
2024-03-27 20:48:24 -0400

    better movement & collisions logged

--- Assets/Scripts/Minigames/Jousting/JoustingCharacter.cs
+++ Assets/Scripts/Minigames/Jousting/JoustingCharacter.cs
@@ -4,13 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JoustingCharacter : MonoBehaviour {
-    /*[SerializeField]
-    private BoxCollider frontCollider;
-    [SerializeField]
-    private BoxCollider backCollider;
-    [SerializeField]
-    private BoxCollider sideCollider;*/
-
     private int playerIndex;
     private float speed;
     private float turnSpeed;
@@ -21,12 +14,25 @@ public class JoustingCharacter : MonoBehaviour {
 
     private JoustingManager manager;
 
-    public bool IsButtonAPressed { get { return bButtonAPressed; } }
+    private int hp = 2;
 
+    private bool canGetHit;
+
+    /* Movement */
+    [SerializeField]
+    private float acceleration = 6f;
+    [SerializeField]
+    private float deceleration = 6f;
+    [SerializeField]
+    private float maxSpeed = 8f;
+    private float currentSpeed = 0f;
+
+    public bool IsButtonAPressed { get { return bButtonAPressed; } }
 
     private void Start() {
         manager = GameObject.Find("JoustingManager").GetComponent<JoustingManager>();
         manager.SetupPlayer(this);
+        canGetHit = true;
     }
 
     public void SetupPlayer(int playerIndex, float speed, float turnSpeed) {
@@ -49,27 +55,45 @@ public class JoustingCharacter : MonoBehaviour {
 
     private void Update() {
         MovePlayer();
+
+        if (hp <= 0) {
+            Debug.Log($"Player {playerIndex + 1} died");
+        }
     }
 
     private void MovePlayer() {
+        // Todo: add acceleration / decceleration
         gameObject.transform.Rotate(0f, direction.x * turnSpeed * Time.deltaTime, 0f);
-        gameObject.transform.Translate(0f, 0f, direction.y * speed * Time.deltaTime);
+
+        if (direction.y > 0) {
+            // Accelerate
+            currentSpeed = Mathf.MoveTowards(currentSpeed, maxSpeed, acceleration * Time.deltaTime);
+        }
+        else {
+            // Deccelerate
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, deceleration * Time.deltaTime);
+        }
+        gameObject.transform.Translate(0f, 0f, direction.y * currentSpeed * Time.deltaTime);
+
     }
 
-    private void OnCollisionEnter(Collision collision) {
-        foreach (ContactPoint contact in collision.contacts) {
-            string colName = contact.thisCollider.name;
-            switch (colName) {
-                case "FrontCollider":
-                    // Block attack
-                    break;
-                case "BackCollider":
-                    // Do full damage
-                    break;
-                case "SideCollider":
-                    // Do half damage
-                    break;
-            }
+    private void OnTriggerEnter(Collider other) {
+        if (!canGetHit) return;
+        Vector3 direction = other.transform.position - transform.position;
+        float dotProduct = Vector3.Dot(transform.forward, direction.normalized);
+        if (dotProduct > 0) {
+            Debug.Log("Trigger Hit from the front");
         }
+        else {
+            Debug.Log("Trigger Hit from the back");
+            hp--;
+            StartCoroutine(HitCoroutine());
+        }
+    }
+
+    IEnumerator HitCoroutine() {
+        canGetHit = false;
+        yield return new WaitForSeconds(1.5f);
+        canGetHit = true;
     }
 }
