38b878edf2d427e924402ffbaeb8ecf2f07862df
Dominic Apgar <dominicapgar2100@gmail.com>
2024-04-05 10:19:48 -0400

    Created Item Dice & Script

---
+++ Assets/Scripts/ItemDiceManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ItemDiceManager : MonoBehaviour
+{
+    public GameObject itemDice;
+    public Transform itemDicePivot;
+    public GameObject itemDiceAnimObj;
+    public Animator itemDiceAnim;
+
+    public bool isRollingItem;
+
+   public TextMeshProUGUI itemDiceName;
+
+    public int result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemDiceAnimObj.SetActive(false);
+        itemDice.SetActive(false);
+        itemDiceName.enabled = false;
+    }
+
+    private void Update()
+    {
+        // Roll dice logic
+        // if (player is on item node)
+        // roll dice
+        StartCoroutine(RollDice());
+    }
+
+    private IEnumerator RollDice()
+    {
+        itemDiceAnimObj.SetActive(true);
+        itemDiceAnim.SetBool("isRolling", true);
+
+        isRollingItem = true;
+
+        int diceRoll;
+        diceRoll = Random.Range(1, 7);
+
+        yield return new WaitForSeconds(2.5f);
+
+        itemDiceAnimObj.SetActive(false);
+        itemDiceAnim.SetBool("isRolling", false);
+
+        itemDice.SetActive(true);
+
+        switch (diceRoll)
+        {
+            case 1:
+                itemDicePivot.Rotate(0, 90, 0);
+                break;
+
+            case 2:
+                itemDicePivot.Rotate(0, 90, -90);
+                break;
+
+            case 3:
+                itemDicePivot.Rotate(0, 180, -90);
+                break;
+
+            case 4:
+                itemDicePivot.Rotate(0, 360, -90);
+                break;
+
+            case 5:
+                itemDicePivot.Rotate(0, 270, -90);
+                break;
+
+            case 6:
+                itemDicePivot.Rotate(0, 270, 0);
+                break;
+
+            default:
+                break;
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        // Stores Dice Roll
+        GameManager.instance.diceRoll = result;
+    }
+}
