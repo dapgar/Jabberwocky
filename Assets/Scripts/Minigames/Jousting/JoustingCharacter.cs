fe3c4010032f41f26b0566a7ef6187e132e3d04a
Zachary Hooper <the1errandboy@gmail.com>
2024-04-09 19:47:32 -0400

    made some slight joust adjustments (feel free to revert)

--- Assets/Scripts/Minigames/Jousting/JoustingCharacter.cs
+++ Assets/Scripts/Minigames/Jousting/JoustingCharacter.cs
@@ -18,14 +18,20 @@ public class JoustingCharacter : MonoBehaviour {
 
     private bool canGetHit;
 
+    [SerializeField]
+    GameObject joustObj;
+
     /* Movement */
     [SerializeField]
-    private float acceleration = 6f;
+    private float acceleration = 12f;
     [SerializeField]
-    private float deceleration = 6f;
+    private float deceleration = 12f;
     [SerializeField]
     private float maxSpeed = 8f;
     private float currentSpeed = 0f;
+
+    private float maxTurnSpeed = 2.4f;
+    private float minTurnSpeed = 1f;
 
     public bool IsButtonAPressed { get { return bButtonAPressed; } }
 
@@ -37,7 +43,7 @@ public class JoustingCharacter : MonoBehaviour {
 
     public void SetupPlayer(int playerIndex, float speed, float turnSpeed) {
         this.playerIndex = playerIndex;
-        this.speed = speed;
+        this.maxSpeed = speed;
         this.turnSpeed = turnSpeed;
     }
 
@@ -63,7 +69,12 @@ public class JoustingCharacter : MonoBehaviour {
 
     private void MovePlayer() {
         // Todo: add acceleration / decceleration
-        gameObject.transform.Rotate(0f, direction.x * turnSpeed * Time.deltaTime, 0f);
+        //gameObject.transform.Rotate(0f, direction.x * turnSpeed * Time.deltaTime, 0f);
+        
+        //transform.forward += transform.forward * -direction.y * currentSpeed * Time.deltaTime * 2;
+
+        //currentSpeed += (20 * direction.y * Time.deltaTime) / 10 + -4 * Time.deltaTime;
+
 
         if (direction.y > 0) {
             // Accelerate
@@ -73,8 +84,27 @@ public class JoustingCharacter : MonoBehaviour {
             // Deccelerate
             currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, deceleration * Time.deltaTime);
         }
-        gameObject.transform.Translate(0f, 0f, direction.y * currentSpeed * Time.deltaTime);
+        
 
+        if (currentSpeed < 0f)
+        {
+            currentSpeed = 0f;
+        }
+
+        // harder to turn at faster speeds
+        turnSpeed = minTurnSpeed + (1 - Mathf.Pow(currentSpeed / maxSpeed, 1)) * maxTurnSpeed;
+        // adjust joust rot base on speed
+        joustObj.transform.localEulerAngles = new Vector3((1 - Mathf.Clamp(currentSpeed / (maxSpeed * .6f), 0, 1)) * -80, joustObj.transform.localEulerAngles.y, joustObj.transform.localEulerAngles.z);
+
+        // add rotation
+        transform.forward += transform.right * direction.x * turnSpeed * Time.deltaTime;
+        
+        // move forward
+        gameObject.transform.localPosition += transform.forward * currentSpeed * Time.deltaTime;
+
+        
+        //transform.Translate(transform.forward * currentSpeed * Time.deltaTime);
+        //Debug.Log(currentSpeed);
     }
 
     private void OnTriggerEnter(Collider other) {
